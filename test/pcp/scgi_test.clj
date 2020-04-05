aee81c2ab0a807aa1074e7b8cabb466cc583364d
Alexander Oloo <alekcz@gmail.com>
2020-04-05 18:43:53 +0200

    wait even longer

--- test/pcp/scgi_test.clj
+++ test/pcp/scgi_test.clj
@@ -11,18 +11,17 @@
     (let [running (atom true)
           handler #(core/scgi-handler %)
           scgi-port 55555
-          _ (do 
-              (Thread/sleep 1000)
-              (future (scgi/serve handler scgi-port running)))
-          socket (Socket. (InetAddress/getByName "127.0.0.1") scgi-port)
+          _ (future (scgi/serve handler scgi-port running))
           message (slurp "test-resources/scgi-request.txt")
           len (count message)]
-      (with-open [^Writer w (io/writer socket)
-                    rdr (io/reader socket)]
-        (.write w message 0 len)
-        (.flush w)
-        (let [ans (slurp rdr)]
-          (is (= "200\r\nContent-Type: text/plain\r\n\r\n1275" ans))
-          (is (true? (.isClosed socket)))
-          (reset! running nil)
-          (Thread/sleep 500))))))
+      (Thread/sleep 4000)
+      (let [socket (Socket. (InetAddress/getByName "127.0.0.1") scgi-port)]
+        (with-open [^Writer w (io/writer socket)
+                      rdr (io/reader socket)]
+          (.write w message 0 len)
+          (.flush w)
+          (let [ans (slurp rdr)]
+            (is (= "200\r\nContent-Type: text/plain\r\n\r\n1275" ans))
+            (is (true? (.isClosed socket)))
+            (reset! running nil)
+            (Thread/sleep 500)))))))

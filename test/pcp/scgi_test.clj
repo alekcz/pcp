092bf811f4a3f12350e74490862d4951cc37aa1e
Alexander Oloo <alekcz@gmail.com>
2020-04-07 10:28:33 +0200

    looking for the hanging

--- test/pcp/scgi_test.clj
+++ test/pcp/scgi_test.clj
@@ -25,31 +25,3 @@
             (is (true? (.isClosed socket)))
             (reset! running nil)
             (Thread/sleep 500)))))))
-
-
-(def t 
-  (Thread. (fn []
-    (try
-      (scgi/serve #(core/scgi-handler %) 22222)
-      (catch InterruptedException e
-        (println (.getMessage e)))))))
-
-(deftest serve-2-test
-  (testing "Test SCGI server"
-    (let [running (atom true)
-          handler #(core/scgi-handler %)
-          scgi-port 55555
-          _ (future (scgi/serve handler scgi-port running))
-          message (slurp "test-resources/scgi-request.txt")
-          len (count message)]
-      (Thread/sleep 500)
-      (let [socket (Socket. (InetAddress/getByName "127.0.0.1") scgi-port)]
-        (with-open [^Writer w (io/writer socket)
-                      rdr (io/reader socket)]
-          (.write w message 0 len)
-          (.flush w)
-          (let [ans (slurp rdr)]
-            (is (= "200\r\nContent-Type: text/plain\r\n\r\n1275" ans))
-            (is (true? (.isClosed socket)))
-            (reset! running nil)
-            (Thread/sleep 500)))))))

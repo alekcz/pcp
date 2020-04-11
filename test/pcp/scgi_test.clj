c0d17c8916ea8a0178c17ea0395758e6723cd048
Alexander Oloo <alekcz@gmail.com>
2020-04-11 21:31:39 +0200

    back scgi request

--- test/pcp/scgi_test.clj
+++ test/pcp/scgi_test.clj
@@ -14,15 +14,14 @@
           scgi-port 55555
           _ (future (scgi/serve handler scgi-port running))
           message (IOUtils/toByteArray (io/input-stream "test-resources/scgi.bin"))
-          len (count message)
-          _ (println (IOUtils/toString message "UTF-8"))]
+          len (count message)]
       (Thread/sleep 500)
       (let [socket (Socket. (InetAddress/getByName "127.0.0.1") scgi-port)]
-        (with-open [^Writer w (io/writer socket)
-                      rdr (io/reader socket)]
-          (.write w message 0 len)
-          (.flush w)
-          (let [ans (slurp rdr)]
+        (with-open [os (io/output-stream (.getOutputStream socket))]
+          (.write os message 0 len)
+          (.flush os)
+          (let [ans (IOUtils/toString (.getInputStream socket))
+                _ (.close socket)]
             (is (= "200\r\nContent-Type: text/plain\r\n\r\n1275" ans))
             (is (true? (.isClosed socket)))
             (reset! running nil)

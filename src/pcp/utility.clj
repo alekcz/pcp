60e2215ea22990a91538d064f7204a30aaecd6e0
Alex Oloo <alekcz@gmail.com>
2021-06-27 22:00:35 +0200

    prep for v0.0.1-beta.22

--- src/pcp/utility.clj
+++ src/pcp/utility.clj
@@ -19,7 +19,7 @@
 
 (def root (atom nil))
 (def scgi (atom "9000"))
-(def version "v0.0.1-beta.21")
+(def version "v0.0.1-beta.22")
 
 (defn keydb []
   (or (env :pcp-keydb) "/usr/local/etc/pcp-db"))
